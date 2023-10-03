74b9b5b298dfe209d214d3a1df791264179f65f5
yeasin50 <yeasinsheikh50@gmail.com>
2023-10-03 23:44:50 +0600

    added-max_width_container

--- packages/responsive/lib/responsive.dart
+++ packages/responsive/lib/responsive.dart
@@ -5,3 +5,4 @@ import 'package:flutter/material.dart';
 part 'src/widgets/responsive_view.dart';
 part 'src/core/responsive_si.dart';
 part 'src/core/screen_type.dart';
+part 'src/widgets/max_width_container.dart';

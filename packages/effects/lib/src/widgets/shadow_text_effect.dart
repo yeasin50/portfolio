03b6cf5aab00b3b7d998cf6a5cc2439f06f57da6
yeasin50 <yeasinsheikh50@gmail.com>
2025-02-26 09:19:31 +0600

    added-constaints

--- packages/effects/lib/src/widgets/shadow_text_effect.dart
+++ packages/effects/lib/src/widgets/shadow_text_effect.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../../effects.dart';
@@ -5,17 +7,25 @@ import '../../effects.dart';
 /// shows shadow opposite of the mouse,
 /// colors and blur fade is proportional to the distance of mouse
 ///
+/// - [maxShadowDistance] handles the shadow distance
+///
 class ShadowTextEffect extends StatefulWidget {
   const ShadowTextEffect({
     super.key,
     required this.text,
     this.style,
     required this.shadowColor,
+    this.maxShadowDistance,
   });
 
   final String text;
   final TextStyle? style;
   final Color shadowColor;
+
+  /// max blur distance from the actual  position
+  /// if null then  it gonna use [style] fontSize->height
+  ///
+  final double? maxShadowDistance;
 
   @override
   State<ShadowTextEffect> createState() => _ShadowTextEffectState();
@@ -32,6 +42,14 @@ class _ShadowTextEffectState extends State<ShadowTextEffect>
         _textKey.currentContext?.findRenderObject() as RenderBox?;
 
     _textWidgetPosition = box?.localToGlobal(Offset.zero);
+    if (_textWidgetPosition == null) return;
+
+    bool isLTR = Directionality.of(context) == TextDirection.ltr;
+
+    _textWidgetPosition = Offset(
+      _textWidgetPosition!.dx + (box!.size.width / 2 * (isLTR ? 1 : -1)),
+      _textWidgetPosition!.dy + (box.size.height / 2 * (isLTR ? 1 : -1)),
+    );
   }
 
   @override
@@ -48,14 +66,30 @@ class _ShadowTextEffectState extends State<ShadowTextEffect>
   Offset shadowOffset = Offset.zero;
   double blurRadius = 0;
 
+  late final double maxShadowDistance = widget.maxShadowDistance ??
+      (widget.style?.fontSize ??
+              Theme.of(context).textTheme.bodyMedium?.fontSize ??
+              14) *
+          2;
+
+  late TextStyle? style = widget.style;
+
+  double lerpFactor = 1;
+
   void onMouseMovement(Offset position) {
     if (_textWidgetPosition == null) return;
 
     Offset direction = _textWidgetPosition! - position;
     double distance = direction.distance;
 
-    shadowOffset = direction / 10;
-    blurRadius = distance / 20;
+    if (distance == 0) return;
+
+    Offset normalizedDirection = direction / distance;
+    lerpFactor = (distance / 100).clamp(0, 1);
+
+    double scaledDistance = lerpDouble(0, maxShadowDistance, lerpFactor)!;
+    shadowOffset = normalizedDirection * scaledDistance;
+
     setState(() {});
   }
 
@@ -64,12 +98,14 @@ class _ShadowTextEffectState extends State<ShadowTextEffect>
     return Text(
       widget.text,
       key: _textKey,
-      style: widget.style?.copyWith(
+      style: style?.copyWith(
         shadows: [
           Shadow(
-            color: widget.shadowColor,
+            color: widget.shadowColor.withAlpha(
+              lerpDouble(0, 150, lerpFactor)!.toInt(),
+            ),
             offset: shadowOffset,
-            blurRadius: blurRadius,
+            blurRadius: lerpDouble(10, 0, lerpFactor)!,
           )
         ],
       ),

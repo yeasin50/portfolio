9bb584ff39e3ab3b7cbe27bd1d491439e9742194
yeasin50 <yeasinsheikh50@gmail.com>
2025-03-18 23:38:07 +0600

    init-so-view

--- packages/stackoverflow_stats/lib/src/presentation/so_profile_view.dart
+++ packages/stackoverflow_stats/lib/src/presentation/so_profile_view.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 
-import 'package:collection/collection.dart';
+import 'package:effects/effects.dart' as eff;
 import 'package:flutter/material.dart';
 import 'package:stackoverflow_stats/stackoverflow_stats.dart';
 
@@ -56,49 +56,151 @@ class _SoProfileViewState extends State<SoProfileView> {
                 widget.description!,
                 style: theme.labelStyle,
               ),
-            Row(
-              spacing: 16,
-              children: user.cardData.entries.mapIndexed(
-                (i, e) {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        e.value.toString(),
-                        textAlign: TextAlign.center,
-                        style: theme.titleStyle,
-                      ),
-                      Text(
-                        e.key,
-                        textAlign: TextAlign.center,
-                        style: theme.labelStyle,
-                      ),
-                      const SizedBox(height: 8),
-                      BadgeCounterView(
-                        badge: SOBadge.empty.copyWith(
-                          rank: switch (i) {
-                            0 => "gold",
-                            1 => "silver",
-                            2 => "bronze",
-                            _ => ""
-                          },
-                          awardCount: switch (i) {
-                            0 => user.goldBadgeCount,
-                            1 => user.silverBadgeCount,
-                            2 => user.bronzeBadgeCount,
-                            _ => 0,
-                          },
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ).toList(),
-            ),
+            _UserStats(profile: snapshot.data!)
           ],
         );
       },
+    );
+  }
+}
+
+class _UserStats extends StatelessWidget {
+  const _UserStats({
+    super.key,
+    required this.profile,
+  });
+  final SoProfile profile;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).extension<StackOverflowTheme>()!;
+
+    List<Widget> children = [];
+
+    final List<SOBadge> badges = [
+      SOBadge.empty.copyWith(
+        rank: "gold",
+        awardCount: profile.goldBadgeCount,
+      ),
+      SOBadge.empty.copyWith(
+        rank: "sliver",
+        awardCount: profile.silverBadgeCount,
+      ),
+      SOBadge.empty.copyWith(
+        rank: "bronze",
+        awardCount: profile.bronzeBadgeCount,
+      ),
+    ];
+    if (profile.reached != null) {
+      children.add(
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              profile.reached!,
+              style: theme.titleStyle.copyWith(
+                fontSize: theme.titleStyle.fontSize! * 2,
+              ),
+            ),
+            Text(
+              "people reached",
+              style: theme.titleStyle,
+            ),
+            const SizedBox(height: 24),
+            _BadgeView(badges: badges),
+          ],
+        ),
+      );
+    }
+
+    children.add(
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            profile.reputation.toString(),
+            style: theme.titleStyle.copyWith(
+              fontSize: theme.titleStyle.fontSize! * 1.5,
+            ),
+          ),
+          Text(
+            "Reputation",
+            style: theme.titleStyle,
+          ),
+        ],
+      ),
+    );
+
+    children.add(
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            profile.totalAnswer.toString(),
+            style: theme.titleStyle.copyWith(
+              fontSize: theme.titleStyle.fontSize! * 1.5,
+            ),
+          ),
+          Text(
+            "answers",
+            style: theme.titleStyle,
+          ),
+        ],
+      ),
+    );
+    children.add(
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            profile.totalQuestion.toString(),
+            style: theme.titleStyle.copyWith(
+              fontSize: theme.titleStyle.fontSize!,
+            ),
+          ),
+          Text(
+            "questions",
+            style: theme.titleStyle,
+          ),
+        ],
+      ),
+    );
+
+    return eff.GoldenSpiralRecursive(
+      size: const Size(400, 400),
+      children: children
+          .map(
+            (e) => ColoredBox(
+              color: Colors.cyan.withAlpha(100),
+              child: eff.SphereView(
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: e,
+                ),
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+}
+
+class _BadgeView extends StatelessWidget {
+  const _BadgeView({
+    super.key,
+    required this.badges,
+  });
+
+  final List<SOBadge> badges;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      spacing: 16,
+      children: [
+        for (final b in badges) BadgeCounterView(badge: b),
+      ],
     );
   }
 }

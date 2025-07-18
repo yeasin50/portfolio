b249e26a0989b55b7bd993bb276a351e3a73c89e
yeasin50 <46500228+yeasin50@users.noreply.github.com>
2025-07-18 07:31:43 +0600

    wip-domain

---
+++ packages/sun_scheduler/lib/src/routine_info.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+///  [RoutineInfo] is the data of RoutineView
+
+/// parse daily time slots
+///
+// {
+//     "start_work" : "05:00 am",
+//     "end_work" : "08:00 am",
+// },
+class TimeSlot {
+  TimeSlot({
+    required this.start,
+    required this.end, //
+  });
+
+  final TimeOfDay start;
+  final TimeOfDay end;
+
+  static parseTimeOfDay(String data) {
+    int? hour = int.tryParse(data.split(":").first);
+    final minuteAm = data.split(":").last.split(" ");
+    final minute = int.tryParse(minuteAm.first);
+    if (hour == null || minute == null) {
+      return null;
+    }
+    if (minuteAm.last.toLowerCase() == "pm") {
+      hour += 12;
+    }
+    return TimeOfDay(hour: hour, minute: minute);
+  }
+
+  // "start_work" : "05:00 am"
+  factory TimeSlot.fromMap(Map<String, dynamic> map) {
+    final startSection = map.keys.firstWhere((e) => e.startsWith("start"));
+    final endSection = map.keys.firstWhere((e) => e.startsWith("end"));
+
+    return TimeSlot(
+      start: parseTimeOfDay(parseTimeOfDay(startSection)),
+      end: parseTimeOfDay(parseTimeOfDay(endSection)),
+    );
+  }
+}
+
+class EntityRow {
+  const EntityRow({
+    required this.name,
+    required this.slot,
+    required this.duration,
+  });
+  final String name;
+  final TimeSlot? slot;
+  final Duration? duration;
+
+  ///
+  factory EntityRow.fromMap(MapEntry e) {
+    TimeSlot? slot;
+    Duration? duration;
+    if (e.value is List) {
+      slot = TimeSlot.fromMap(e.value);
+    } else if (e.value is int) {
+      duration = Duration(days: e.value);
+    }
+    return EntityRow(name: e.key, slot: slot, duration: duration);
+  }
+}
+
+class RoutineInfo {
+  RoutineInfo({
+    required this.title,
+    required this.headers,
+    required this.timezone,
+    required this.entities,
+  });
+
+  final String title;
+  final List<String> headers;
+  final String timezone;
+  final List<EntityRow> entities;
+
+  factory RoutineInfo.fromMap(Map<String, dynamic> map) {
+    final data = map["entities"] as Map<String, dynamic>;
+
+    final result = data.entries.map((e) => EntityRow.fromMap(e)).toList();
+
+    return RoutineInfo(
+      title: map["title"],
+      headers: List.from(map["headers"]),
+      timezone: map['timezone'],
+      entities: result,
+    );
+  }
+}

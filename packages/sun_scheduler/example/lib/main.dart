2690d81d9c8c002b551ced16c33562e628ced202
yeasin50 <46500228+yeasin50@users.noreply.github.com>
2025-07-20 07:23:16 +0600

    feat:timezone-parser

--- packages/sun_scheduler/example/lib/main.dart
+++ packages/sun_scheduler/example/lib/main.dart
@@ -1,19 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:sun_scheduler/sun_scheduler.dart';
+import 'package:time_machine2/time_machine2.dart' as tm;
+import 'package:flutter_timezone/flutter_timezone.dart' as tz;
 
 void main() {
-  runApp(const MainApp());
+    WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(home: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<String> availableTimezone = [];
+  String? myTimezone;
+  List<String> filterTimezone = [];
+
+  final seachController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    tm.TimeMachine.initialize().then((_) {
+      fetchLocalDB();
+    });
+
+    seachController.addListener(searchlistner);
+  }
+
+  void fetchLocalDB() async {
+    availableTimezone = await tz.FlutterTimezone.getAvailableTimezones();
+    filterTimezone = [...availableTimezone];
+    myTimezone = await tz.FlutterTimezone.getLocalTimezone();
+
+    setState(() {});
+  }
+
+  void searchlistner() {
+    final text = seachController.text.trim();
+
+    if (text.isEmpty) {
+      filterTimezone = availableTimezone;
+    } else {
+      filterTimezone =
+          availableTimezone
+              .where((e) => e.toLowerCase().contains(text.toLowerCase()))
+              .toList();
+    }
+
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    seachController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF323956),
-        body: Center(
+    return Scaffold(
+      backgroundColor: const Color(0xFF323956),
+      body: DefaultTextStyle(
+        style: TextStyle(color: Colors.white),
+        child: Center(
           child: Padding(
             padding: EdgeInsets.all(24.0),
             child: Column(
@@ -21,40 +76,72 @@ class MainApp extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               spacing: 24,
               children: [
-                Align(alignment: Alignment.centerLeft, child: TimeZoneChip()),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: RoutineView(
-                    title: "Daily Schedule",
-                    headers: ["Day", "work-hour", "meeting"], //
-                    entity: [
-                      //row
-                      ["Saturday", "05:00-9:00", ""],
-                      ["Saturday", "05:00-9:00", "12:22-01:00"],
-                      ["Saturday", "05:00-9:00", ""],
-                      ["Saturday", "05:00-9:00", ""],
-                    ],
+                Text("Your timezone :$myTimezone"),
+                if (availableTimezone.isEmpty)
+                  Text("loading....")
+                else
+                  Expanded(
+                    child: Column(
+                      children: [
+                        SearchBar(controller: seachController),
+                        Expanded(
+                          child: ListView.separated(
+                            itemCount: filterTimezone.length,
+                            separatorBuilder: (_, _) => SizedBox(height: 8),
+                            itemBuilder: (contex, i) {
+                              return ListTile(
+                                title: Text(
+                                  filterTimezone[i],
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: RoutineView(
-                    title: "Holidays",
-                    headers: ["Event", "period"], //
-                    entity: [
-                      //row
-                      ["Ramadan", "12April,2025 - 12May,2025"],
-                      ["Ramadan", "12April,2025 - 12May,2025"],
-                      ["Eid-Ul-Fitor", "05:00-9:00"],
-                    ],
-                  ),
-                ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget exampleUI() {
+    return Column(
+      children: [
+        Align(alignment: Alignment.centerLeft, child: TimeZoneChip()),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: RoutineView(
+            title: "Daily Schedule",
+            headers: ["Day", "work-hour", "meeting"], //
+            entity: [
+              //row
+              ["Saturday", "05:00-9:00", ""],
+              ["Saturday", "05:00-9:00", "12:22-01:00"],
+              ["Saturday", "05:00-9:00", ""],
+              ["Saturday", "05:00-9:00", ""],
+            ],
+          ),
+        ),
+
+        Align(
+          alignment: Alignment.centerLeft,
+          child: RoutineView(
+            title: "Holidays",
+            headers: ["Event", "period"], //
+            entity: [
+              //row
+              ["Ramadan", "12April,2025 - 12May,2025"],
+              ["Ramadan", "12April,2025 - 12May,2025"],
+              ["Eid-Ul-Fitor", "05:00-9:00"],
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
